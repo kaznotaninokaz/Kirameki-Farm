8eb9e968b1269f3310a33ccfd39427c5e1b7b61f
kaznotaninokaz <kazzzzz0358@gmail.com>
2025-06-01 13:55:05 +0900

    first commit

---
+++ assets/script/main.js
@@ -0,0 +1,14 @@
+// ハンバーガーメニュー
+var hamburger = $('.hamburger__menu');
+// OPEN/CLOSEボタンをクリックしたら
+$('.hamburger__button').on('click', function () {
+  // console.log('click');
+  // .hamburgerの表示・非表示を繰り返す
+  hamburger.toggleClass('hamburger__menu-active');
+});
+// 画面幅のサイズが変わったら
+$(window).on('resize', function () {
+  // console.log('resize');
+  // ハンバーガーメニューを閉じる
+  hamburger.removeClass('hamburger__menu-active');
+});

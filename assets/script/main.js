4b3782facebfa7b85fe97149997e128e599fd486
kaznotaninokaz <kazzzzz0358@gmail.com>
2025-06-04 21:00:33 +0900

    3ページ目完成

--- assets/script/main.js
+++ assets/script/main.js
@@ -22,3 +22,36 @@ $('.products-service__slick').slick({
   infinite: false,
 });
 
+// アコーディオンのタイトルがクリックされたら
+$('.faq__item-content').on('click', function (e) {
+
+    // .question-content-accordion-bottomを選択
+    var content = $(this).next();
+
+    // .question-content-accordion-bottomを表示・非表示
+    content.slideToggle();
+
+    //目印となるクラスを追加
+    $(this).toggleClass('close');
+
+      // 開いているアコーディオンがあるかどうかをチェック
+    const anyOpen = $('.faq__item-content.close').length > 0;
+
+    // 現在のウィンドウ幅を取得
+    const windowWidth = $(window).width();
+
+    // 画面幅と開閉状態によって row-gap を切り替え
+    if (windowWidth <= 768) {
+        if (anyOpen) {
+            $('.faq__container').css('row-gap', '1.1875rem');
+        } else {
+            $('.faq__container').css('row-gap', '1.5rem');
+        }
+    } else {
+        if (anyOpen) {
+            $('.faq__container').css('row-gap', '1.375rem');
+        } else {
+            $('.faq__container').css('row-gap', '1.375rem');
+        }
+    }
+});
